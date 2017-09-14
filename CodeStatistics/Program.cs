5494efc0966af2615940e091da2090b2e4d396b1
chylex <contact@chylex.com>
2017-09-14 21:36:39 +0200

    Hello world (again)

---
+++ CodeStatistics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace CodeStatistics{
+    static class Program{
+        public static string Version { get; } = typeof(Program).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+        private static void Main(string[] args){
+            Console.Title = "Code Statistics "+Version;
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
